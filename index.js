69a9636415196754094db38dded1335d66fd92a4
madelinedulchin <madeline.dulchin@gmail.com>
2022-09-30 15:36:34 -0400

    commit tree d1f9fdb07c8d5ac906076b79431d98ea54c9dacc

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+const findDrivers = function(drivers, string){
+ let name = drivers.filter(element => element === string)
+ return name
+    }
+
+
+// function findMatching(drivers, strings){
+//     drivers.filter((names => {
+
+//     })
+         
+//     }
